1024c446a8da636e8ad03aa103ef1785c57afc17
code538 <ranjan1992kp@gmail.com>
2024-12-04 23:35:00 +0530

    topbar logo

--- resources/views/employeer/include/topbar.blade.php
+++ resources/views/employeer/include/topbar.blade.php
@@ -30,7 +30,7 @@
     <div class="header-left">
         <a href="{{ url('organization/employerdashboard') }}" class="logo" style="display: block; width: 100%; height: 100%;">
             @if(!empty($company_details->logo))
-            <img src="{{asset('storage/' . $company_details->logo)}}" alt="Company Logo" style="width: auto; height: 40px; object-fit: contain;">
+            <img src="{{asset('storage/app/public/' . $company_details->logo)}}" alt="Company Logo" style="width: auto; height: 40px; object-fit: contain;">
 
             <!-- <img src="{{asset('storage/app/public/' . $company_details->logo)}}" alt="Company Logo" style="width: 100%; height: 100%; object-fit: cover;"> -->
             @endif
