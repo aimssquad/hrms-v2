1e8d4c358fa2b610beaaffa7fc7510ef3e8d95b8
code538 <ranjan1992kp@gmail.com>
2025-03-10 19:07:56 +0530

    email code change

--- resources/views/sub-admin/index.blade.php
+++ resources/views/sub-admin/index.blade.php
@@ -15,7 +15,7 @@
         <!-- Favicon -->
         <link rel="shortcut icon" type="image/x-icon" href="{{ asset('frontend/assets/img/swch_logo.png') }}">
     @else
-        <title>Login -{{$domain_name->com_name}}</title>
+        <title>Login</title>
         <!-- Favicon -->
         {{-- <link rel="shortcut icon" type="image/x-icon" href="{{ asset('storage/app/public/' . $domain_name->logo) }}"> --}}
         @if($domain_name && $domain_name->logo) 
