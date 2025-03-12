de75c0aadfd59e0902e9d72137dbd17fffc655a2
code538 <ranjan1992kp@gmail.com>
2025-03-13 00:00:06 +0530

    email code change

--- resources/views/email-authentication.blade.php
+++ resources/views/email-authentication.blade.php
@@ -43,7 +43,7 @@
 					
 					<div class="account-box">
 						<div class="account-wrapper">
-							<p class="account-title">Please log in using the email <strong>{{ session('email') }}</strong> </p>
+							<p class="account-title">Please log in using the email</p>
 							<p class="account-title"><strong>{{ session('email') }}</strong></p>
 							<p class="account-title"><strong></strong>and enter the OTP provided below.</p>
 							<p class="account-subtitle">@include('employeer.layout.message')</p>
