cd0c92f4fffecaf29258519c186655ed49351886
code538 <ranjan1992kp@gmail.com>
2025-03-15 19:06:25 +0530

    email code change

--- resources/views/mailre.blade.php
+++ resources/views/mailre.blade.php
@@ -1,5 +1,5 @@
 <p>Hello <b>Superadmin</b>,</p>
-@if($user_type)
+@if($user_type == "Partner")
   <p>This is new Partner details: </p>
 @else
   <p>This is new @if($org_code) <b>Partner</b> @endif <b>Organisation</b> details: </p>
