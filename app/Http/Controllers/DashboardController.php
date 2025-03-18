51fcdd93037e500978a5c58b872d37effd6725b9
code538 <ranjan1992kp@gmail.com>
2025-03-18 23:29:22 +0530

    email code change

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4252,10 +4252,8 @@ Furthermore, disciplinary action may be taken against you. You must inform the m
 
             $data = array('com_name' => $Roledata->com_name, 'com_logo' => $Roledata->logo, 'address' => $Roledata->address . ',' . $Roledata->address2 . ',' . $Roledata->road, 'addresssub' => $Roledata->city . ',' . $Roledata->zip . ',' . $Roledata->country, 'Roledata' => $Roledata, 'offer' => $job);
 
-            //$toemail = $job->emp_ps_email;
-            $toemail = "tuhinmukherjee55@gmail.com";
+            $toemail = $job->emp_ps_email;
             // return view('reminder-email-90days', $data);
-            // dd($toemail);
             Mail::send('reminder-email-90days', $data, function ($message) use ($toemail) {
                 $message->to($toemail)->subject('Your Visa is Due to Expire in 90 Days');
                 $message->from(env('MAIL_USERNAME'));
