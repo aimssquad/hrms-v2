941aec661189de6d3892a43e808a18a3b8ec926d
code538 <ranjan1992kp@gmail.com>
2025-04-10 16:59:38 +0530

    billing code push

--- resources/views/orgInvoicePdf.blade.php
+++ resources/views/orgInvoicePdf.blade.php
@@ -11,9 +11,9 @@
                 {{-- <img src="https://ik.imagekit.io/oq9hcqjih/main-logo.png?updatedAt=1733651934565" alt="Logo" style="height: 50px;"> --}}
                 @if($logo)
                     <img src="{{asset('storage/app/public/'.$p_logo)}}" alt="Logo" style="height: 50px;">
-                @endif
-                    <h1>{{ strtoupper($p_com_name) }}</h1>
                 @else
+                    <h1>{{ strtoupper($p_com_name) }}</h1>
+                @endif
                 
             </td>
             <td colspan="4" style="text-align: right; border: none; padding: 10px;">
