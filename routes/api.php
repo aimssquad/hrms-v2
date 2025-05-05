c44a226a34d10969acbdd421e672702e6f6a07aa
code538 <ranjan1992kp@gmail.com>
2025-05-05 16:51:37 +0530

    ifsc code push

--- routes/api.php
+++ routes/api.php
@@ -56,7 +56,7 @@ Route::group(['prefix' => 'v1', 'middleware' => ['auth:api']], function () {
 });
 
 Route::group(['prefix' => 'v1', 'middleware' => ['auth:api']], function () { 
-    Route::post('holiday-list', [HolidayController::class, 'nationalHoliday']); 
+    Route::get('holiday-list', [HolidayController::class, 'nationalHoliday']); 
     Route::get('holiday-type',[HolidayController::class, 'getHolidayType']);
     Route::post('holiday-apply', [HolidayController::class, 'applyHoliday']);
 });
