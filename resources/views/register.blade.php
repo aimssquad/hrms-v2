d373d7788b5dc04e33fc1b437e4e6c4fd7a7f2a6
code538 <ranjan1992kp@gmail.com>
2025-07-06 15:39:56 +0530

    sidebar code push

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -163,7 +163,7 @@
                         <div class="text-center">
                            <div class="account-logo">
                             @if($dName ==='swcworlds.com' || $dName === 'skilledworkerscloud.co.uk') 
-                                <a href="https://skilledworkerscloud.co.uk/hrms-v2/"><img src="{{asset('frontend/assets/img/sponicHR-logo.png')}}" alt="SponicHR"></a>
+                                <a href="https://skilledworkerscloud.co.uk/hrms-v2/"><img src="{{asset('frontend/assets/img/sponicHr-logo.png')}}" alt="SponicHR"></a>
                             @else 
                                 @if($domain_name && $domain_name->logo) 
                                     <a href="{{ asset('storage/app/public/' . $domain_name->logo) }} "><img src="{{ asset('storage/app/public/' . $domain_name->logo) }}" alt="{{$domain_name->com_name}}" 
