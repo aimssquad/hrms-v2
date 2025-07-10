b0796c305c30046b6b283c395a87b6f166a5e6b8
code538 <ranjan1992kp@gmail.com>
2025-07-10 17:34:05 +0530

    bulk employee insert

--- resources/views/employeer/employee/sync-employee-upload.blade.php
+++ resources/views/employeer/employee/sync-employee-upload.blade.php
@@ -113,7 +113,10 @@
                                  <button type="submit" class="btn btn-primary">
                                     <i class="fas fa-upload mr-2"></i> Import Data
                                  </button>
-                                 <a href="{{ asset('bulk-employee-csv/bulk_employee_entry.csv') }}" class="btn btn-link">
+                                 <a href="{{ asset('bulk-employee-csv/bulk_employee_entry.csv') }}" 
+                                    class="btn btn-link"
+                                    download="employee_import_sample.csv"
+                                    target="_blank">
                                     <i class="fas fa-download mr-1"></i> Download Sample CSV
                                  </a>
                            </div>
