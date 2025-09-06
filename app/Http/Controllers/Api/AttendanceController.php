d59411306067c0cd5800fdaf025e1283cc6cf256
code538 <ranjan1992kp@gmail.com>
2025-09-06 19:01:35 +0530

    attandance api code

--- app/Http/Controllers/Api/AttendanceController.php
+++ app/Http/Controllers/Api/AttendanceController.php
@@ -1466,7 +1466,7 @@ class AttendanceController extends Controller
                 $date = date('Y-m-d');
                 
                 // Base query
-                $attendance = Attandence::where('employee_code', $employee_id)
+                $attendance = TempAttendance::where('employee_code', $employee_id)
                                     ->where('emid', $emid)
                                     ->where('date', $date)
                                     ->orderBy('id','desc')
